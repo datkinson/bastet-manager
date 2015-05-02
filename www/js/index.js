9fe0cee5daceefa929db41fdcf58698842f9fa9e
Daniel Atkinson <hourd.tasa@gmail.com>
2015-05-02 21:39:13 +0100

    initial test application

---
+++ www/js/index.js
@@ -0,0 +1,23 @@
+// setup new pouch database
+var db = new PouchDB('kittens');
+
+// create a new document
+var doc = {
+  "_id": "mittens",
+  "name": "Mittens",
+  "occupation": "kitten",
+  "age": 3,
+  "hobbies": [
+    "playing with balls of yarn",
+    "chasing laser pointers",
+    "lookin' hella cute"
+  ]
+};
+
+// put document in the database
+db.put(doc);
+
+// get document out and console log it
+db.get('mittens').then(function (doc) {
+  console.log(doc);
+});
